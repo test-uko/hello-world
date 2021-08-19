d45dbc96422f8c13f367f99853859b3c9c23a7d9
yuka <yuka.tmp@gmail.com>
2021-08-19 14:47:27 +0900

    add scripts

---
+++ python/test.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import logging
+
+
+logger = logging.getLogger(u"test")
+
+
+if __name__ == u"__main__":
+    print(u"hello-world")
